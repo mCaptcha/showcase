cf98d474ae72f1875a3c9c0e8b7e3e75660cd140
Aravinth Manivannan <realaravinth@batsense.net>
2023-10-28 03:14:09 +0530

    feat: update deps

--- build.rs
+++ build.rs
@@ -1,19 +1,8 @@
-/*
- * Copyright (C) 2021  Aravinth Manivannan <realaravinth@batsense.net>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2021  Aravinth Manivannan <realaravinth@batsense.net>
+// SPDX-FileCopyrightText: 2023 Aravinth Manivannan <realaravinth@batsense.net>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 use std::process::Command;
 
 use sqlx::types::time::OffsetDateTime;
@@ -21,12 +10,13 @@ use sqlx::types::time::OffsetDateTime;
 fn main() {
     // note: add error checking yourself.
     let output = Command::new("git")
-        .args(&["rev-parse", "HEAD"])
+        .args(["rev-parse", "HEAD"])
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 
-    let now = OffsetDateTime::now_utc().format("%y-%m-%d");
+    let now = OffsetDateTime::now_utc();
+    let now = format!("{}{}{}", now.year(), now.month(), now.date());
     println!("cargo:rustc-env=COMPILED_DATE={}", &now);
 }
